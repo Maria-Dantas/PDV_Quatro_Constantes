8e3fb8f026197490c7f77a3a7fd8c51ae8f729a4
Samy <samyceles14@gmail.com>
2023-10-10 23:41:06 -0300

    Instalação do jwt e inicio de rota login

--- src/rotas.js
+++ src/rotas.js
@@ -5,9 +5,13 @@ const { listarCategorias,criarUsuario } = require('./controladores/usuarios')
 
 const rotas = express()
 
+rotas.post('/login', loginUsuario)
 
 rotas.get('/categoria', listarCategorias)
+
 rotas.post('/usuarios',criarUsuario)
+
+
 
 
 
