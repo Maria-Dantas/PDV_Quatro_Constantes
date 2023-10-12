5b52f9c2c5e17f068dc1f00ef50cd57fcc68bc1c
JulianaT93 <juliana.toguti@gmail.com>
2023-10-11 21:32:24 -0300

    Implementação das rotas detalhar usuário, alterar usuário, implementação do intermediário e refatoração feito em conjunto com Deise e Maria

---
+++ src/intermediarios/autenticacao.js
@@ -0,0 +1,41 @@
+require('dotenv').config();
+const jwt = require('jsonwebtoken');
+const { erroServidor, erroAutenticacao } = require('../servicos/mensagens');
+const { selecionarUsuariosId } = require('../servicos/consultas-usuarios');
+
+
+const verificarUsuarioLogado = async (req, res, next) => {
+    const { authorization } = req.headers;
+
+    if (!authorization) {
+        return res.status(401).json(erroAutenticacao[0]);
+    };
+
+    const token = authorization.split(' ')[1];
+
+    try {
+
+        const { id } = jwt.verify(token, process.env.JWT);
+
+        const usuarioEncontrado = await selecionarUsuariosId(id);
+
+        if (!usuarioEncontrado) {
+            return res.status(404).json(erroAutenticacao[1]);
+        };
+
+        const { senha, ...usuario } = usuarioEncontrado;
+
+        req.usuario = usuario;
+
+        next();
+
+    } catch (error) {
+        if (error.message === "invalid token") {
+            return res.status(401).json(erroAutenticacao[0]);
+        } else {
+            return res.status(500).json(erroServidor);
+        }
+    };
+}
+
+module.exports = verificarUsuarioLogado;
--- src/rotas.js
+++ src/rotas.js
@@ -1,25 +1,29 @@
 const express = require('express')
 
-const { listarCategorias, criarUsuario, loginUsuario } = require('./controladores/usuarios')
+const { listarCategorias, criarUsuario, loginUsuario, cadastrarUsuario, detalharUsuario, atualizarUsuario } = require('./controladores/usuarios')
 const { checarCamposCadastro, checarCamposLogin, checarDuplicidadeEmail } = require('./intermediarios/validacao')
 
-const { listarCategorias, cadastrarUsuario } = require('./controladores/usuarios')
-
+const verificarUsuarioLogado = require('./intermediarios/autenticacao');
 
 
 
 const rotas = express()
 
+rotas.post('/usuario', cadastrarUsuario)
+
 rotas.post('/login', checarCamposLogin, loginUsuario)
 
-rotas.post('/usuarios', cadastrarUsuario)
+//rotas.post('/usuarios', cadastrarUsuario)
 
 rotas.get('/categoria', listarCategorias)
 
 
-rotas.post('/usuarios', checarCamposCadastro, checarDuplicidadeEmail, criarUsuario)
+//rotas.post('/usuarios', checarCamposCadastro, checarDuplicidadeEmail, criarUsuario)
 
+rotas.use(verificarUsuarioLogado);
 
+rotas.get('/usuario', detalharUsuario);
+rotas.put('/usuario', atualizarUsuario);
 
 
 module.exports = rotas   
